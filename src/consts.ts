886ca82c7570521b91a10ee74fe71a62f69ac321
Sergey Fomchukov <durovam06@gmail.com>
2025-01-10 20:33:18 +0700

    Dev init (10.01.2025)

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Серегапомоги!!!';
+export const SITE_DESCRIPTION = 'Добро пожаловать на сайт, где Вы можете изучить что-то новое или закрепить старое.';
